a88cd847ab1b36197b7a72942d2772e4fab59053
Robert Pepple <bob.pepple@comcast.net>
2017-06-07 11:19:05 -0700

    Tweaked styling for score table to add better responsive behavior

--- assets/app/ghinDisplay/ghin-display.component.ts
+++ assets/app/ghinDisplay/ghin-display.component.ts
@@ -2,15 +2,7 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-ghin-display',
-  templateUrl: `
-        <div class="row">
-         <div class="col-md-12">
-          <h2 id="ghinDisplay_h2">Calculated GHIN for {{userFirstName}}</h2>
-          <br>
-          <p id="ghinDisplay">{{ghinText}}</p>
-         </div>
-        </div>
-       `,
+  templateUrl: './ghin-display.component.html',
   styleUrls: ['./ghin-display.component.css']
 })
 export class GhinDisplayComponent {
