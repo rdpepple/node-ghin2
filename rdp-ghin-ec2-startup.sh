dc6bf9a40bded3c19331d0ad930ba5526c39ae14
Robert Pepple <bob.pepple@comcast.net>
2017-07-31 19:28:58 -0700

    Fixed small issue in ec2 startup shell

--- rdp-ghin-ec2-startup.sh
+++ rdp-ghin-ec2-startup.sh
@@ -2,8 +2,7 @@
 yum update -y
 sudo yum install git -y
 sudo yum install -y gcc gcc-c++ make openssl-devel
-[ ! -d /ghin-app ] && mkdir -p /ghin-app
-# Mount for EFS volume - sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-983fe131.efs.us-west-2.amazonaws.com:/ /ghin-app
+[ ! -d /ghin-app ] && mkdir -p /ghin-app && sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-983fe131.efs.us-west-2.amazonaws.com:/ /ghin-app
 cd /tmp
 curl -O https://nodejs.org/dist/v6.11.1/node-v6.11.1.tar.gz
 tar -xf node-v6.11.1.tar.gz && rm -f node-v6.11.1.tar.gz
@@ -16,17 +15,17 @@ sudo ln -s /usr/local/lib/node /usr/lib/node
 sudo ln -s /usr/local/bin/npm /usr/bin/npm
 cd ..
 rm -fv node-v6.11.1/
-mkdir ghin-app
-cd ghin-app
+mkdir -p /tmp/ghin-app-prod
+cd /tmp/ghin-app-prod
 git clone http://github.com/rdpepple/node-ghin2 .
-mkdir ghin-app-prod
-mv ghin-app.tar.gz ghin-app-prod
-cd ghin-app-prod
+mkdir ghin-tar
+mv ghin-app.tar.gz ghin-tar
+cd ghin-tar
 tar -xf ghin-app.tar.gz && rm -f ghin-app.tar.gz
 cd ..
-rsync -War ./ghin-app-prod/ /ghin-app/
+rsync -War ./ghin-tar/ /ghin-app/
 cd ..
-rm -rf ghin-app
+rm -rf ghin-app-prod
 cd /ghin-app
 npm install
 echo "Instance is healthy" >/ghin-app/healthy.html
